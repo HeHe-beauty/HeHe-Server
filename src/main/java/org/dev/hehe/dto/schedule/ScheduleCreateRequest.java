610c9b1137639da6e7bbc34e7830874b06f9ffed
노태환 <thnoh93@naver.com>
2026-04-19 01:41:21 +0900

    fix schedule api swagger

--- src/main/java/org/dev/hehe/dto/schedule/ScheduleCreateRequest.java
+++ src/main/java/org/dev/hehe/dto/schedule/ScheduleCreateRequest.java
@@ -1,5 +1,6 @@
 package org.dev.hehe.dto.schedule;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 /**
@@ -9,14 +10,18 @@ import lombok.Getter;
  * - userId는 JWT에서 자동 추출 (@LoginUser)
  */
 @Getter
+@Schema(description = "일정 추가 요청")
 public class ScheduleCreateRequest {
 
     /** 병원명 (필수) */
+    @Schema(description = "병원명", example = "ㅇㅇㅇ의원", requiredMode = Schema.RequiredMode.REQUIRED)
     private String hospitalName;
 
     /** 시술명 (선택) */
+    @Schema(description = "시술명", example = "레이저 제모", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String procedureName;
 
     /** 방문 예정 시각 (Unix timestamp, seconds, 필수) */
+    @Schema(description = "방문 시각 (unix timestamp - millisecond)", example = "1777024800", requiredMode = Schema.RequiredMode.REQUIRED)
     private Long visitTime;
 }
